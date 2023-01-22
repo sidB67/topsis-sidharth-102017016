caa9ebdcf0eb14bf7d301b600b477e30340f755b
sidB67 <sidbahl67@gmail.com>
2023-01-22 20:38:08 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## SaralGyaan Square
+### It takes an integer as an input and prints it square.
+
+### Installation
+## pip install saral-square
+
+### How to use it?
+## Open terminal and type square and then input the integer
+
+## License
+### © 2023 Sidharth Bahl
+
+### This repository is licensed under the MIT license. See LICENSE for details.
---
+++ setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="topsis-sidharth-102017016",
+    version="0.1.0",
+    description="It finds topsis for the given data in csv file",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/uditvashisht/saral-square",
+    author="Sidharth Bahl",
+    author_email="bahlsidharth@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["topsis-sidharth"],
+    include_package_data=True,
+    install_requires=["pandas", "numpy"],
+    entry_points={
+        "console_scripts": [
+            "topsis-sidharth=topsis-sidharth.__main__:main",
+        ]
+    },
+)
